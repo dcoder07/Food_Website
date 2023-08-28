5079189b09710933b43028963d0a8ecf213f9577
Dishank Patil <dishankjp05@gmail.com>
2023-08-28 10:41:35 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
+const menu = document.querySelector("#menu");
+const burger = document.querySelector("#burger");
 
-burger.addEventListener('click',() => {
-if(menu.classList.contains('hidden')){
-    menu.classList.remove('hidden');
-}
-else{
-    menu.classList.add('hidden');
-}
-
+burger.addEventListener("click", ()=>{
+ if(menu.classList.contains("hidden")){
+menu.classList.remove("hidden");
+ }
+ else{
+    menu.classList.add("hidden");
+ } 
 })
